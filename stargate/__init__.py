32e06edf7ab68cbbcefc767eb29bb0fd4be21d26
reaper <148925959+DavidWise01@users.noreply.github.com>
2026-08-24 11:53:56 -0500

    Export Ricky star-gate key

---
+++ stargate/__init__.py
@@ -0,0 +1,15 @@
+from .ricky import (
+    RICKY_SOURCE,
+    MandelbrotWanderer,
+    RickyKey,
+    WanderStep,
+    mandelbrot_escape,
+)
+
+__all__ = [
+    "RICKY_SOURCE",
+    "MandelbrotWanderer",
+    "RickyKey",
+    "WanderStep",
+    "mandelbrot_escape",
+]
