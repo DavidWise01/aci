ba256623decf4a92fbc8a48eaf39ea981f79450f
reaper <148925959+DavidWise01@users.noreply.github.com>
2026-08-24 23:32:25 -0500

    Export Cysaphis rolling chain

--- stargate/__init__.py
+++ stargate/__init__.py
@@ -2,8 +2,14 @@ from .ricky import (
     RICKY_NAME, RICKY_PATTERN, RICKY_PATTERN_TEXT,
     RickyKey, WanderStep, issue_ricky_key, roll_pattern,
 )
+from .cysaphis import (
+    CYSAPHIS_NAME, CYCLE_LENGTH,
+    RollBrick, CysaphisChain, issue_cysaphis,
+)
 
 __all__ = [
     "RICKY_NAME", "RICKY_PATTERN", "RICKY_PATTERN_TEXT",
     "RickyKey", "WanderStep", "issue_ricky_key", "roll_pattern",
+    "CYSAPHIS_NAME", "CYCLE_LENGTH",
+    "RollBrick", "CysaphisChain", "issue_cysaphis",
 ]
